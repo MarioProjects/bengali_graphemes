92a262382fc4bee7e2ac406d58f1c3df7bee8b33
MarioProjects <maparla@inf.upv.es>
2020-01-22 20:32:54 +0100

    magic seresnext -> stride2 to maxpool

--- models/seresnext.py
+++ models/seresnext.py
@@ -33,6 +33,57 @@ class Seresnext101(nn.Module):
 
         return x1, x2, x3
 
+
+class MagicSeresnext101(nn.Module):
+    def __init__(self, head, classes=[], ps=0.5, pretrained=True):
+        super().__init__()
+
+        model = ptcv_get_model("seresnext101_32x4d", pretrained=pretrained)
+
+        modules = {}
+        for name, module in model.named_modules():
+            if (isinstance(module, nn.Conv2d)):
+                stride = module.stride
+                if stride == (2, 2) or stride == 2:
+                    module.stride = (1, 1)
+                    modules[name] = module
+                elif stride == 2:
+                    module.stride = 1
+                    modules[name] = module
+
+        for name in modules:
+            parent_module = model
+            objs = name.split(".")
+            if len(objs) == 1:
+                # model.__setattr__(name, modules[name])
+                model.__setattr__("magicMaxPool", nn.MaxPool2d(kernel_size=2, stride=2))
+                continue
+
+            for obj in objs[:-1]:
+                parent_module = parent_module.__getattr__(obj)
+
+            # parent_module.__setattr__(objs[-1], modules[name])
+            parent_module.__setattr__("magicMaxPool", nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.features = nn.Sequential(*list(model.features.children())[:-1])
+
+        nc = 2048
+
+        self.head1 = head(nc, classes[0], ps=ps)
+        self.head2 = head(nc, classes[1], ps=ps)
+        self.head3 = head(nc, classes[2], ps=ps)
+        ## to_Mish(self.layer0), to_Mish(self.layer1), to_Mish(self.layer2)
+        ## to_Mish(self.layer3), to_Mish(self.layer4)
+
+    def forward(self, x):
+        x = self.features(x)
+
+        x1 = self.head1(x)
+        x2 = self.head2(x)
+        x3 = self.head3(x)
+
+        return x1, x2, x3
+
 class Seresnext101Banglalekha(nn.Module):
     def __init__(self, head, classes=[84], ps=0.5, pretrained=True):
         super().__init__()
